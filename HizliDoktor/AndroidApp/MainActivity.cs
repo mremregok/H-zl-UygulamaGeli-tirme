c1abf3062c58f5a24f35b1dab4aa0a149923b6ab
Ersoy TORAMAN <jovannijares@gmail.com>
2019-05-09 14:37:21 +0300

    temel activty eklendi.

--- HizliDoktor/AndroidApp/MainActivity.cs
+++ HizliDoktor/AndroidApp/MainActivity.cs
@@ -4,17 +4,58 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 using Autofac;
+using Business.Abstract;
+using Android.Content;
 
 namespace AndroidApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private Button btnLogin, btnUyeOl;
+        private EditText txtTC, txtPass;
+        ILoginService loginService;
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            loginService = Business.IOCUtil.Container.Resolve<ILoginService>();
+
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            btnUyeOl = FindViewById<Button>(Resource.Id.btnUyeOl);
+            txtTC = FindViewById<EditText>(Resource.Id.txtTC);
+            txtPass = FindViewById<EditText>(Resource.Id.txtPass);
+
+            btnLogin.Click += BtnLogin_Click;
+            btnUyeOl.Click += BtnUyeOl_Click;
+        }
+
+        private void BtnUyeOl_Click(object sender, System.EventArgs e)
+        {
+            //üye ol activity çağır
+            var intent = new Intent(this, typeof(RandevuActivity));
+            StartActivity(intent);
+        }
+
+        private void BtnLogin_Click(object sender, System.EventArgs e)
+        {
+            bool isLoggedIn = loginService.Login("tc", "pass");
+
+            if (isLoggedIn)
+            {
+                //randevu alma activity çağır
+                var intent = new Intent(this, typeof(RandevuActivity));
+                intent.PutExtra("tc", txtTC.Text);
+                StartActivity(intent);
+            }
+            else
+            {
+                Toast.MakeText(Application.Context, "Giriş yapılamadı. Lütfen bilgilerinizi kontrol edin.", ToastLength.Long);
+            }
         }
     }
 }
---
+++ HizliDoktor/Business/Concrete/LoginManager.cs
@@ -0,0 +1,15 @@
+﻿using Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class LoginManager : ILoginService
+    {
+        public bool Login(string TC, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
