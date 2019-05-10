949118242531277efba87921aa2676bb26dfa1db
mremregok <46620582+mremregok@users.noreply.github.com>
2019-05-10 11:08:20 +0300

    Update ProfileActivity.cs
    View'lerin çekilmesi işlemi.

--- HizliDoktor/AndroidApp/ProfileActivity.cs
+++ HizliDoktor/AndroidApp/ProfileActivity.cs
@@ -1,25 +1,60 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using Autofac;
+using Business.Abstract;
+using Entities.Concrete;
 
 namespace AndroidApp
 {
-    [Activity(Label = "ProfileActivity")]
+    [Activity(Label = "ProfileActivity", Theme = "@style/AppTheme")]
     public class ProfileActivity : Activity
     {
+        IHastaService hastaService;
+
+        private TextView hastaAd, hastaSoyad, hastaTCKN, hastaCinsiyet, hastaAciklama, hastaDogumTarihi;
+
+        private GridView hastaGecmisRandevular;
+
+        private Button hastaProfiliGuncelle;
+
+        public ProfileActivity()
+        {
+            hastaService = Business.IOCUtil.Container.Resolve<IHastaService>();
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.profile_layout);
             // Create your application here
+
+            hastaAd = FindViewById<TextView>(Resource.Id.lblHastaAdi);
+
+            hastaSoyad = FindViewById<TextView>(Resource.Id.lblHastaSoyadi);
+
+            hastaTCKN = FindViewById<TextView>(Resource.Id.lblHastaTCKN);
+
+            hastaCinsiyet = FindViewById<TextView>(Resource.Id.lblHastaCinsiyet);
+
+            hastaAciklama = FindViewById<TextView>(Resource.Id.lblHastaAciklama);
+
+            hastaDogumTarihi = FindViewById<TextView>(Resource.Id.lblHastaDogumTarihi);
+
+            hastaGecmisRandevular = FindViewById<GridView>(Resource.Id.gridGecmisRandevular);
+
+            hastaProfiliGuncelle = FindViewById<Button>(Resource.Id.btnHastaBilgiDuzenle);
+
+
+
         }
     }
 }
