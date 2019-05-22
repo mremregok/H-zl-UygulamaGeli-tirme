f822b4a74cd6d4f8162d0239573cd8efa6575666
Ersoy TORAMAN <jovannijares@gmail.com>
2019-05-23 01:44:43 +0300

    admin randevu kapatma tamamlandı.

--- HizliDoktor/AndroidApp/AdminRandevuKapandiActivity.cs
+++ HizliDoktor/AndroidApp/AdminRandevuKapandiActivity.cs
@@ -13,22 +13,22 @@ using Android.Widget;
 
 namespace AndroidApp
 {
-    [Activity(Label = "Randevu kapatıldı.", Theme = "@style/AppTheme")]
+    [Activity(Label = "Randevu kapatıldı", Theme = "@style/AppTheme")]
     public class AdminRandevuKapandiActivity : AppCompatActivity
     {
         Button btnRandevuKapatDon;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.randevuOnaylandi_layout);
+            SetContentView(Resource.Layout.adminRandevuKapandi_layout);
 
             btnRandevuKapatDon = FindViewById<Button>(Resource.Id.btnRandevuKapatDon);
 
-            btnRandevuKapatDon.Click += BtnRandevuKapatDon_Click;
+            btnRandevuKapatDon.Click += BtnRandevuKapatDon_Click1;
             // Create your application here
         }
 
-        private void BtnRandevuKapatDon_Click(object sender, EventArgs e)
+        private void BtnRandevuKapatDon_Click1(object sender, EventArgs e)
         {
             var intent = new Intent(this, typeof(AdminRandevuKapatActivity));
             StartActivity(intent);
